915f8c3dee1c6aa0f62090d396753a141fec7252
LoiolaBr17 <mateustaua16@gmail.com>
2022-10-12 14:38:03 -0300

    Creating page and login form

--- src/components/Pages/Login/Login.jsx
+++ src/components/Pages/Login/Login.jsx
@@ -1,9 +1,48 @@
+import { Link } from "react-router-dom"
+
+import Input from "../../form/Input/Input"
+
+import '../../Form/FormCSS/styles.css'
+
+
+
 const Login = () => {
-    return (
-      <div className="App">
-        <h1>Login</h1>
-      </div>
-    )
+
+  const handleChange = () => {
+
+  }
+
+  const handleSubmit = () => {
+
+  }
+
+  return (
+    <section className="form_container">
+        <h1>Entrar na Área Administrativa</h1>
+
+        <form onSubmit={handleSubmit}>
+            <Input 
+              text = "E-mail"
+              type = "email"    
+              name = "email"
+              placeholder = "Digite o seu email"
+              handleOnChange={handleChange}
+            />
+            <Input 
+              text = "Senha"
+              type = "password"    
+              name = "password"
+              placeholder = "Digite a sua senha"
+              handleOnChange={handleChange}
+            />
+            
+            <input type="submit" value="Entrar" />
+        </form>
+
+        <p>Não é um administrador ? <Link to='/'>Clique aqui.</Link></p>
+    </section>
+)
+    
 }
-  
+
 export default Login
