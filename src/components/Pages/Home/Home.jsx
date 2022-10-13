f147691836c2ed2441cdc0a75458a2d12a101ec5
LoiolaBr17 <mateustaua16@gmail.com>
2022-10-13 06:27:48 -0300

    rescuing data from public notices on home

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,12 +1,33 @@
+import { useState,useEffect } from 'react'
+
+import api from '../../../utils/api'
+
 import './styles.css'
 
 /* Components */
 import Presentation from '../../Presentation/Presentation'
 
 const Home = () => {
+    const [edicts, setEdicts] = useState([])
+
+    useEffect(() => {
+
+      api.get('notices')
+      .then((response) => {
+        setEdicts(response.data.notices)
+        console.log(edicts.length)
+      })
+
+    }, [])
+
     return (
       <section>
-        <Presentation />
+        <div>
+          <Presentation />
+        </div>
+        <div>
+          {edicts.length === 0 && <p>Não há editais cadastrados.</p>}
+        </div>
       </section>
     )
   }
