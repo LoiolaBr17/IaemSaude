62710682e14059614e7b5fe8eebdd4fc88cbcf04
LoiolaBr17 <mateustaua16@gmail.com>
2022-10-14 13:29:00 -0300

    creating the delete functionality

--- src/components/Pages/Manager/Manager.jsx
+++ src/components/Pages/Manager/Manager.jsx
@@ -1,18 +1,92 @@
 import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 
+import useFlashMessage from '../../../hooks/useFlashMessage'
+
+import api from "../../../utils/api"
+import Axios from "axios"
+
+import './styles.css'
+
 const Manager = () => {
-    const [Edict, setEdict] = useState([])
+  const [edicts, setEdicts] = useState([])
+  const [token] = useState(localStorage.getItem('token') || '')
+  const {setFlashMessage} = useFlashMessage()
+
+  useEffect(() => {
+
+    api.get('notices?limit=50')
+    .then((response) => {
+      setEdicts(response.data.notices)
+    })
+
+  }, [])
+
+  const removeEdict = async (id) => {
+    let msgType = 'success'
+    let msgText = 'Edital removido com sucesso'
+
+    var axios = Axios.create()
+        
+        await axios({
+            method: 'delete',
+            responseType: 'json',
+            headers: {
+                Authorization: `${JSON.parse(token)}`
+            },
+            url: `https://api.comsentimento.com.br/notices/${id}`,
+            
+        })
+        .then(response => {
+             console.log(response)
+             msgText = "Edital removido com sucesso" 
+             msgType = "success"
+        })
+        .catch(error => {
+            msgText = "Não foi possível remover o edital" 
+            msgType = "error"
+        });
+
+      // const data = await api.delete(`/pets/${id}`, {
+      //   headers: {
+      //     Authorization: token,
+      //   }
+      // })
+      // .then((response) => {
+      //   const updatedEdicts = edicts.filter((edict) =>edict.noticeID != id)
+      //   setEdicts(updatedEdicts)
+      //   console.log(response)
+      // })
+      // .catch((err) => {
+      //   msgText = 'Erro ao remover o Edital'
+      //   msgType = 'error'
+      //   console.log(err)
+      // })
+      
+
+      setFlashMessage(msgText,msgType)
+    }
 
     return (
       <section>
-        <div>
+        <div className="manager_header">
           <h1>Editais Cadastrados</h1>
           <Link to="/edict/add">Cadastrar Edital</Link>
         </div>
-        <div>
-          {Edict.length > 0 && <p>Editais Cadastrados</p>}
-          {Edict.length === 0 && <p>Não há editais cadastrados</p>}
+
+        <div className="manager_container">
+          {edicts && 
+            edicts.map((edict,index) => (
+              <div key={index} className='manager_row'>
+                <span className="bold">Edital: {edict.noticeTitle}</span>
+                <div className="actions">
+                  <Link to={`/edict/edit/${edict.noticeID}`}>Editar</Link>
+                  <button onClick={() => {removeEdict(edict.noticeID)}}>Excluir</button>
+                </div>
+              </div>
+            ))
+          }
+          {edicts.length === 0 && <p>Não há editais cadastrados</p>}
         </div>
       </section>
     )
