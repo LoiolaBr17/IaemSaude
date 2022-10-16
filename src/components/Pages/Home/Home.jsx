2bee06801a9d08297efa5e63841b66c49be73175
LoiolaBr17 <mateustaua16@gmail.com>
2022-10-16 12:36:37 -0300

    adding pagination in

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -10,16 +10,32 @@ import ContainerEdicts from '../../ContainerEdicts/ContainerEdicts'
 
 const Home = () => {
     const [edicts, setEdicts] = useState([])
+    const [currentPage, setCurrentPage] = useState(0)
+    const [total, setTotal] = useState(0) 
+    const [displacement, setDisplacement] = useState(0)
+
+    const pages = Math.ceil(total / 10)
 
     useEffect(() => {
 
-      api.get('notices?limit=50')
+      api.get(`notices?limit=10&offset=${displacement}`)
       .then((response) => {
         setEdicts(response.data.notices)
+        setTotal(response.data.total)
         console.log(response.data.notices)
       })
 
-    }, [])
+    }, [displacement])
+
+    const setPage = (index) => {
+      const displacement = 10 * index
+      setCurrentPage(index)
+      setDisplacement(displacement)
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    }
 
     return (
       <section>
@@ -28,6 +44,22 @@ const Home = () => {
         </div>
         <div>
           {edicts && (<ContainerEdicts edicts={edicts}/>) }
+          
+          <div id='home_btn_pagination'>
+            {Array.from(Array(pages), (item, index) => {
+              return (
+                <button 
+                  onClick={() => setPage(index)} 
+                  className="btn_pagination" 
+                  value={index} 
+                  key={index}
+                  style={index === currentPage ? {backgroundColor: "#604fff", color: "#fff"}: null}
+                >
+                  {index+1}
+                </button> 
+              )
+            })}
+          </div>
         </div>
       </section>
     )
